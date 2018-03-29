dbdd35ec512eb71aec9b062a966193d63eea7665
Alex Wahlberg <alexjwahlberg@gmail.com>
2018-03-29 18:45:50 -0500

    added

--- js/my.js
+++ js/my.js
@@ -1,18 +1,22 @@
-$.ajax({
-	type: 'GET',
-	url: 'https://eg-shows.herokuapp.com/',
-	async: 'true',
-	dataType: 'json',
-	beforeSend: function () {
-		// We should show an AJAX spinner here
-	},
-	complete: function () {
-		// And then hide the AJAX spinner here
-	},
-	success: function (result) {
-		console.log(result);
-	},
-	error: function (jqXHR, textStatus, errorThrown) {
-		console.log('There was an error');
-	}
+$(document).ready(function(){
+
+
+	$.ajax({
+		type: 'GET',
+		url: 'https://eg-shows.herokuapp.com/',
+		async: 'true',
+		dataType: 'json',
+		beforeSend: function () {
+			// We should show an AJAX spinner here
+		},
+		complete: function () {
+			// And then hide the AJAX spinner here
+		},
+		success: function (result) {
+			console.log(result);
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			console.log('There was an error');
+		}
+	});
 });
