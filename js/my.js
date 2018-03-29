bc2006260395d41ef424c4b37d677deaf925e865
Alex Wahlberg <alexjwahlberg@gmail.com>
2018-03-29 18:41:22 -0500

    js update
    added cory's magic

--- js/my.js
+++ js/my.js
@@ -1,5 +1,18 @@
-
-
-if ($(window).width() < 768) {
-	$('.new-break').replaceWith('<br>');
-}
+$.ajax({
+	type: 'GET',
+	url: 'https://eg-shows.herokuapp.com/',
+	async: 'true',
+	dataType: 'json',
+	beforeSend: function () {
+		// We should show an AJAX spinner here
+	},
+	complete: function () {
+		// And then hide the AJAX spinner here
+	},
+	success: function (result) {
+		console.log(result);
+	},
+	error: function (jqXHR, textStatus, errorThrown) {
+		alertDialog('There was an error');
+	}
+});
